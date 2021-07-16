22b70e68d65cac6e362af3d9424c4fde2e571eab
henry weli <henrysnoops@gmail.com>
2021-07-16 14:48:34 +0100

    upgraded the wsendpoint handler to a websocket connection

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
@@ -10,6 +12,17 @@ var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./templates/html"),
 	jet.InDevelopmentMode(), // remove in production
 )
+
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	err:= renderPage(w, "home.jet", nil)
@@ -28,5 +41,29 @@ func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 	views.Execute(w, data, nil)
 	return nil
 
+}
 
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request)  {
+	ws, err:= upgrader.Upgrade(w,r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("connection to websocket was successful")
+
+	var response WsJsonResponse
+
+	response.Message = `<em><small>connected to the server</small></em>`
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+	}
+	
+	
 }
